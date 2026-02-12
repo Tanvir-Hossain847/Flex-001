87e579dfef7242c986f3a6e4445c280a4397e0b7
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2026-02-12 21:43:31 +0600

    feat: add problem-solution section with animations

--- flex/src/app/page.jsx
+++ flex/src/app/page.jsx
@@ -1,13 +1,15 @@
 import Hero from "@/components/Hero";
 import ProductDetailSection from "@/components/ProductDetailSection";
 import VisualShowcase from "@/components/VisualShowcase";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <ProductDetailSection />
-      <VisualShowcase />
+      <Hero></Hero>
+      <WhyChooseUs></WhyChooseUs>
+      <ProductDetailSection></ProductDetailSection>
+      <VisualShowcase></VisualShowcase>
     </main>
   );
 }
