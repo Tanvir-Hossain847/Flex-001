c64ec5c194210db15238a866403d343d8644f84b
Arman Mir <mirarman8583@gmail.com>
2026-02-12 11:06:41 +0600

    Hero refactored

--- flex/src/app/page.jsx
+++ flex/src/app/page.jsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/Hero";
 import ProductDetailSection from "@/components/ProductDetailSection";
 import VisualShowcase from "@/components/VisualShowcase";
 
 export default function Home() {
   return (
     <main>
+      <Hero />
       <ProductDetailSection />
       <VisualShowcase />
     </main>
